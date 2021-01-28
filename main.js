19d00b5dc850e9b30bf6e1a7eb7d8542bca1f6e9
Emmanuel Santiago <biosinx@gmail.com>
2021-01-27 20:00:36 -0500

    operadores_comparacion_27_01_2021

---
+++ main.js
@@ -0,0 +1,51 @@
+//Operador igualdad
+let a = 50;
+let b = 100;
+
+console.log(`Operador igualdad ${a == b}`);
+
+//Operador diferencia
+let c = 20;
+let d = 100;
+
+console.log(`Operador diferencia ${c != d}`);
+
+//Operador identica
+let e = '100';
+let f = '100';
+
+console.log(`Tipo de dato variable e ${typeof(e)} valor del dato ${e}`);
+console.log(`Tipo de dato variable f ${typeof(f)} valor del dato ${f}`);
+console.warn(`Resultado de la comparacion ${e === f}`);
+
+//Operador identica
+let g = '100';
+let h = 50;
+
+console.log(`Tipo de dato variable g ${typeof(g)} valor del dato ${g}`);
+console.log(`Tipo de dato variable h ${typeof(h)} valor del dato ${h}`);
+console.warn(`Resultado de la comparacion ${g !== h}`);
+
+//Operador mayor
+let i = 50;
+let j = 40;
+
+console.log(`El dato ${i} es mayor que ${j}, ${i > j}`);
+
+//Operador mayor o igual
+let k = 50;
+let l = 50;
+
+console.log(`El dato ${k} es mayor o igual que ${l}, ${k >= l}`);
+
+//Operador menor
+let m = 50;
+let n = 40;
+
+console.log(`El dato ${m} es menor que ${n}, ${m < n}`);
+
+//Operador menor o igual
+let o = 50;
+let p = 50;
+
+console.log(`El dato ${o} es menor o igual que ${p}, ${o <= p}`);
